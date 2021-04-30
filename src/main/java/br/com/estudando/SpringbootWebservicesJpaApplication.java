34a1bdb9807500f47f82df1651475cba87d2a143
Giovani Silva <giovani-silva1@live.com>
2021-04-30 11:25:56 -0300

    Adicionado o projeto inicial do Spring boot

---
+++ src/main/java/br/com/estudando/SpringbootWebservicesJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicesJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudando/SpringbootWebservicesJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebservicesJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
